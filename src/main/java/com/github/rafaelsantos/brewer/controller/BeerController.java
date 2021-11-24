dd76fb72f4f3b046edd315aa63858a3b304dddec
Rafael Santos <santos.rafaelbs@gmail.com>
2021-11-24 16:39:23 -0300

    Configuring ViewResolver with Thymeleaf

--- src/main/java/com/github/rafaelsantos/brewer/controller/BeerController.java
+++ src/main/java/com/github/rafaelsantos/brewer/controller/BeerController.java
@@ -1,8 +1,13 @@
 package com.github.rafaelsantos.brewer.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BeerController {
 
+	@RequestMapping("/beer/add")
+	public String add() {
+		return "beer/add";
+	}
 }
