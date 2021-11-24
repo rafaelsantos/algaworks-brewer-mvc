3b43ad82e1579bde3c1424bf17013d1f93a75c54
Rafael Santos <santos.rafaelbs@gmail.com>
2021-11-24 17:13:50 -0300

    Creating controller action passing single parameter to view

--- src/main/java/com/github/rafaelsantos/brewer/controller/BeerController.java
+++ src/main/java/com/github/rafaelsantos/brewer/controller/BeerController.java
@@ -2,12 +2,19 @@ package com.github.rafaelsantos.brewer.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class BeerController {
 
 	@RequestMapping("/beer/add")
 	public String add() {
+		return "beer/add";
+	}
+	
+	@RequestMapping(value = "/beer/add", method = RequestMethod.POST)
+	public String save(String sku) {
+		System.out.println("POST " + sku); 
 		return "beer/add";
 	}
 }
