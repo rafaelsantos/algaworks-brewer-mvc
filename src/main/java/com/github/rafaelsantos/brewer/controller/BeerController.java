2a58b4d3d5cc88d02e3094f2fc66eb717202413c
Rafael Santos <santos.rafaelbs@gmail.com>
2021-11-25 20:14:50 -0300

    Keeping form data

--- src/main/java/com/github/rafaelsantos/brewer/controller/BeerController.java
+++ src/main/java/com/github/rafaelsantos/brewer/controller/BeerController.java
@@ -5,8 +5,10 @@ import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.github.rafaelsantos.brewer.model.Beer;
@@ -15,19 +17,17 @@ import com.github.rafaelsantos.brewer.model.Beer;
 public class BeerController {
 
 	@RequestMapping("/beer/add")
-	public String add(Model model) {
-		model.addAttribute(new Beer());
+	public String add(Beer beer) {
 		return "beer/add";
 	}
 	
 	@RequestMapping(value = "/beer/add", method = RequestMethod.POST)
 	public String save(@Valid Beer beer, BindingResult result, Model model, RedirectAttributes attributes) {
 		if (result.hasErrors()) {
-			return "beer/add";
+			return add(beer);
 		}
 		
 		attributes.addFlashAttribute("message", "Data saved successfully!");
-		System.out.println(beer.getSku()); 
 		return "redirect:/beer/add";
 	}
 }
