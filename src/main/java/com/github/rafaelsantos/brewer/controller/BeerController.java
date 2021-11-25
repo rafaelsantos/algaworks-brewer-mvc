20278a9a671b19fe47cba834c858d136b511ca77
Rafael Santos <santos.rafaelbs@gmail.com>
2021-11-25 09:05:49 -0300

    Working with foward and redirect

--- src/main/java/com/github/rafaelsantos/brewer/controller/BeerController.java
+++ src/main/java/com/github/rafaelsantos/brewer/controller/BeerController.java
@@ -3,9 +3,11 @@ package com.github.rafaelsantos.brewer.controller;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.github.rafaelsantos.brewer.model.Beer;
 
@@ -18,11 +20,14 @@ public class BeerController {
 	}
 	
 	@RequestMapping(value = "/beer/add", method = RequestMethod.POST)
-	public String save(@Valid Beer beer, BindingResult result) {
-		if (result.hasErrors())
-			System.out.println("Error");
+	public String save(@Valid Beer beer, BindingResult result, Model model, RedirectAttributes attributes) {
+		if (result.hasErrors()) {
+			model.addAttribute("message", "Error");
+			return "beer/add";
+		}
 		
-		System.out.println("POST " + beer.getSku()); 
-		return "beer/add";
+		attributes.addFlashAttribute("message", "Data saved successfully!");
+		System.out.println(beer.getSku()); 
+		return "redirect:/beer/add";
 	}
 }
