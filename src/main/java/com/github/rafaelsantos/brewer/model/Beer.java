4716c651c5438e0bfc0257ef16ceec7254f61d8a
Rafael Santos <santos.rafaelbs@gmail.com>
2021-12-13 13:53:52 -0300

    Working with JPA callbacks using PrePersist and PreUpdate annotations

--- src/main/java/com/github/rafaelsantos/brewer/model/Beer.java
+++ src/main/java/com/github/rafaelsantos/brewer/model/Beer.java
@@ -11,6 +11,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 import javax.validation.constraints.DecimalMax;
 import javax.validation.constraints.DecimalMin;
@@ -73,6 +75,11 @@ public class Beer {
 	@ManyToOne
 	@JoinColumn(name = "type_code")
 	private Type type;
+	
+	@PrePersist @PreUpdate
+	private void prePersistUpdate() {
+		sku = sku.toUpperCase();
+	}
 	
 	public String getSku() {
 		return sku;
