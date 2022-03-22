2710f516bf682969b4dfa199eb6dda78afdde40e
rafaelsantos <rafael.balieiro@luizalabs.com>
2022-03-22 14:43:08 -0300

    Sorting the list by SKU or Name

--- src/main/java/com/github/rafaelsantos/brewer/controller/page/PageWrapper.java
+++ src/main/java/com/github/rafaelsantos/brewer/controller/page/PageWrapper.java
@@ -5,6 +5,9 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
+
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -45,4 +48,35 @@ public class PageWrapper<T> {
     public String urlToPage(int page) {
         return uri.replaceQueryParam("page", page).build(true).encode().toUriString();
     }
+
+    public String sort(String field) {
+        UriComponentsBuilder uriSort = UriComponentsBuilder.fromUriString(uri.build(true).encode().toUriString());
+
+        String sort = String.format("%s,%s", field, invert(field));
+        return uriSort.replaceQueryParam("sort", sort).build(true).encode().toUriString();
+    }
+
+    public String invert(String field) {
+        String direction = "asc";
+
+        Order order = page.getSort() != null ? page.getSort().getOrderFor(field) : null;
+
+        if (order != null)
+            direction = Sort.Direction.ASC.equals(order.getDirection()) ? "desc" : "asc";
+
+        return direction;
+    }
+
+    public boolean desc(String field) {
+		return invert(field).equals("asc");
+	}
+	
+	public boolean sorted(String field) {
+		Order order = page.getSort() != null ? page.getSort().getOrderFor(field) : null; 
+		
+		if (order == null)
+			return false;
+		
+		return page.getSort().getOrderFor(field) != null ? true : false;
+	}
 }
